12be83221116836cd95023c6d242ede6f1df2174
Diana <dinguyen@mills.edu>
2016-03-15 12:51:43 -0700

    beginning webapp -- testing home

--- views.py
+++ views.py
@@ -4,15 +4,27 @@ from .forms import webFizzForm
 
 app = Flask(__name__)
 
+from twilio.rest import TwilioRestClient
+
+# Get these credentials from http://twilio.com/user/account
+account_sid = "ACd31f04c5fd89bf0fd1a52a36bcb9b50c"
+auth_token = "575f13c3c92ec55501ed32e1776c5184"
+client = TwilioRestClient(account_sid, auth_token)
+
 @app.route("/", methods=['POST'])
 def menu():
-    form = webFizzForm()
     """Respond to incoming requests."""
     resp = twilio.twiml.Response()
     resp.say("Hello. Let's play PhoneFizz.")
     with resp.gather(finishOnKey="#", action="/beginfizz") as g:
         g.say("Please enter a number to play phonefizz then pressed pound.")
-    return render_template('webfizz.html', form=form)
+    return str(resp)
+
+@app.route("/home")
+def home():
+    form = webFizzForm()
+    return render_template('webfizz.html', form = form)
+
 
 
 """
@@ -63,6 +75,24 @@ def _play_phonefizz(response):
         g.say("Please enter a number to play phonefizz ",
               voice="alice", language="en-GB", loop=2)
     return response
+
+
+@app.route("/webfizz", methods=['GET', 'POST'])
+def webfizz():
+    to_call = request.form['phonenumber']
+    resp = twilio.twiml.Response()
+    if to_call == "":
+        resp.say("Sorry, please enter a phone number.")
+        return redirect("/")
+    else:
+        call = client.calls.create(to=to_call,
+                           from_="+17606426823",
+                           url="/beginfizz")
+    return str(resp)
+
+# Make the call
+
+print call.sid
 
 if __name__ == "__main__":
     app.run(debug=True)
