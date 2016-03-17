b444dc896b0b42570741d825e043f7de4c18c6e0
Diana <dinguyen@mills.edu>
2016-03-16 23:51:37 -0700

    beginning webapp -- starting webfizz

--- run.py
+++ run.py
@@ -28,6 +28,11 @@ def beginfizz():
     return str(resp)
 
 
+@app.route("/webfizz", methods=['GET', 'POST'])
+def webfizz():
+    return render_template('/webfizz.html', form=webFizzForm)
+
+
 def phonefizz(inp):
     output = ""
     for x in range(1, inp+1):
@@ -40,6 +45,24 @@ def phonefizz(inp):
         else:
             output += str(x)+" "
     return output
+
+
+@app.route('/dial', methods=['GET', 'POST'])
+def dial():
+    dest_number = request.form['phonenumber']
+    resp = twilio.twiml.Response()
+    if dest_number == "":
+        resp.say("Sorry, please enter a phone number.")
+        return redirect("/")
+    else:
+        with resp.dial(callerId=caller_id) as r:
+            # If we have a number, and it looks like a phone number:
+            if dest_number and re.search('^[\d\(\)\- \+]+$', dest_number):
+                r.number(dest_number)
+            else:
+                resp.say("Sorry, that is an invalid number.")
+
+    return str(resp)
 
 
 if __name__ == "__main__":
